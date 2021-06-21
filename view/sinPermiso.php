63b24a3f7ca3f73fe3235e94f6eb750eb07279df
Laura <Laura@mail.com>
2021-06-21 09:56:32 -0300

    permisos - borro clase usuariosModel. Usamos usuarioModel - arreglo footer - agrego vista sinPermiso.php - agrego modelo Permiso - agrego archivo de constantes para utilizar con los permisos

---
+++ view/sinPermiso.php
@@ -0,0 +1,20 @@
+{{> header}}
+
+<main>
+    <a href="logout.php"><input type="submit" value="Cerrar sesion"></a>
+    <div>
+        <nav class="menu">
+            <ul class="listaMenu">
+                <li><a href="#">VIAJES</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">VEHÍCULOS</a>
+                </li>
+                <li><a href="#">EMPLEADOS</a></li>
+                <li><a href="#">USUARIOS</a></li>
+            </ul>
+        </nav>
+    </div>
+</main>
+
+<h3> Usted no tiene permiso para acceder a esta sección </h3>
+{{> footer}}
